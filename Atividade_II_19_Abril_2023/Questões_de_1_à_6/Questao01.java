83bdadae0fab5fc7757d2528de5e6793ba19288f
João Gonçalves <113616809+joaojaocme@users.noreply.github.com>
2023-04-29 22:34:18 -0300

    Add files via upload

---
+++ Atividade_II_19_Abril_2023/Questões_de_1_à_6/Questao01.java
@@ -0,0 +1,11 @@
+package Questão01;
+
+public class Questao01 {
+	public static void main(String[] args) {
+		int quant = 1;
+		for(int i = 1;i <= 50;i++) {
+			System.out.println(quant);
+			quant++;
+		}
+	}
+}
