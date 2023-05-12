8e2bebd6db9b3b10aa720896573eef73307dd67f
João Gonçalves <113616809+joaojaocme@users.noreply.github.com>
2023-05-12 18:38:39 -0300

    Add files via upload

---
+++ Meu(s)_Primeiro(s)_Programa(s)/OrientadoÀObjeto/Users.java
@@ -0,0 +1,28 @@
+package OrientadoÀObjeto;
+
+public class Users {
+
+    //Propriedades
+    private String firstName;
+    private String lastName;
+
+
+    //Métodos()
+
+    //SETTERS
+    public void setFirstName(String ola) {
+        //Deixa os caracteres de firstName todos maiúsculos
+        this.firstName = ola.toUpperCase();
+    }
+
+    public void setLastName(String lastName) {
+        //Deixa os caracteres de lastName todos minúsculos
+        this.lastName = lastName.toLowerCase();
+    }
+
+
+    //GETTER
+    public void getCompleteName(){
+        System.out.println(firstName + " " + lastName);
+    }
+}
